72627838a126102b1815792fdaa6e4fd75b4e888
nandinibaride <nandinibaride1234@gmail.com>
2021-08-28 16:00:26 +0530

    Added: welcome

---
+++ src/main/java/addressbook/com/App.java
@@ -0,0 +1,13 @@
+package addressbook.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
